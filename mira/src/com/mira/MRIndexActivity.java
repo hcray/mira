cfc23a08e5721bd340c6048758f2f1d858318295
caiyingying <caiyingying234@gmail.com>
2015-06-28 15:35:42 +0800

    拍照图标的添加

--- mira/src/com/mira/MRIndexActivity.java
+++ mira/src/com/mira/MRIndexActivity.java
@@ -1,76 +1,42 @@
 package com.mira;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
-import android.webkit.WebChromeClient;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-
-import com.common.MRCommon;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class MRIndexActivity extends Activity {
-	WebView webView;
+	
+	/**
+	 * 相机 
+	 */
+	private ImageButton ibCamera;
+	
+	/**
+	 * 日期选择
+	 */
+	private ImageButton ibCalender;
+	
+	/**
+	 * 图片展示
+	 */
+	private ViewPager viewPager;
+	
+	/**
+	 * 日期显示
+	 */
+	private EditText etSelectDate;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.mr_activity_index);
 		
-		/**
-		webView=(WebView)findViewById(R.id.webView);
+		ibCamera = (ImageButton) this.findViewById(R.id.ib_camera);
 		
-		webView.getSettings().setAllowFileAccess(true);// 设置允许访问文件数据
-		webView.getSettings().setJavaScriptEnabled(true);
-		webView.getSettings().setBuiltInZoomControls(true);
-		webView.getSettings().setSupportZoom(true); 
-		webView.getSettings().setLoadWithOverviewMode(true);
-		webView.getSettings().setDomStorageEnabled(true);
-		webView.setWebChromeClient(new WebChromeClient(){
-			
-		});
-		webView.setWebViewClient(new WebViewClient(){
-			@Override
-			public boolean shouldOverrideUrlLoading(WebView view, String url) {
-				if(url.contains("jump://"))
-				{
-					url=url.replace("jump://", "http://");
-					if(url.contains(".taobao.com/"))
-					{
-						url=url.replace("http://", "taobao://");
-						if(MRCommon.isAvilible(MRIndexActivity.this, "com.taobao.taobao"))
-						{
-							Intent intent= new Intent(); 
-                            intent.setAction(Intent.ACTION_VIEW);
-//                            intent.addCategory(Intent.CATEGORY_DEFAULT);// 
-//                            intent.addCategory(Intent.CATEGORY_BROWSABLE);
-                            Uri content_url = Uri.parse(url);  
-                            intent.setData(content_url);  
-                            startActivity(intent);
-						}
-						else
-						{
-							Intent intent=new Intent(MRIndexActivity.this, MRWebViewActivity.class).putExtra("url", url);
-							MRIndexActivity.this.startActivity(intent);
-						}
-					}
-					else
-					{
-						Intent intent=new Intent(MRIndexActivity.this, MRWebViewActivity.class).putExtra("url", url);
-						MRIndexActivity.this.startActivity(intent);
-					}
-				}
-				else
-				{
-					view.loadUrl(url);
-				}
-				return true;
-			}
-		});
-		webView.loadUrl("http://apinet.miramask.com/app");
-		*/
+		
 	}
 
 	@Override
