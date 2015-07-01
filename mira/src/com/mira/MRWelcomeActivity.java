09d5d8256fd581f2f660c357115e5cca832aab4f
caiyingying <caiyingying234@gmail.com>
2015-07-01 14:42:32 +0800

    启动页面渐变效果的添加，修复启动后点击可以返回启动页面的bug

--- mira/src/com/mira/MRWelcomeActivity.java
+++ mira/src/com/mira/MRWelcomeActivity.java
@@ -1,17 +1,42 @@
 package com.mira;
 
-import android.os.Bundle;
-import android.os.Handler;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
 
 public class MRWelcomeActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		final View view = View.inflate(this, R.layout.mr_activity_welcome, null);
+		//LinearLayout wellcome = (LinearLayout) view.findViewById(R.id.app_start_view);
 		setContentView(R.layout.mr_activity_welcome);
+		
+		
+		//渐变展示启动屏
+		AlphaAnimation aa = new AlphaAnimation(0.3f,1.0f);
+		aa.setDuration(2000);
+		view.startAnimation(aa);
+		aa.setAnimationListener(new AnimationListener()
+		{
+			@Override
+			public void onAnimationEnd(Animation arg0) {
+				redirectTo();
+			}
+			@Override
+			public void onAnimationRepeat(Animation animation) {}
+			@Override
+			public void onAnimationStart(Animation animation) {}
+			
+		});
+		/*
 		new Handler(getMainLooper()).postDelayed(new Runnable() {
 			
 			@Override
@@ -20,6 +45,7 @@ public class MRWelcomeActivity extends Activity {
 				
 			}
 		}, 2000);
+		*/
 	}
 
 	@Override
@@ -28,5 +54,14 @@ public class MRWelcomeActivity extends Activity {
 		getMenuInflater().inflate(R.menu.mrwelcome, menu);
 		return true;
 	}
+	
+    /**
+     * 跳转到...
+     */
+    private void redirectTo(){        
+        Intent intent = new Intent(this, MRMainActivity.class);
+        startActivity(intent);
+        finish();
+    }
 
 }
