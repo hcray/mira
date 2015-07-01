8afede73b3161bbed94ad084c56cb32359493451
caiyingying <caiyingying234@gmail.com>
2015-07-01 17:27:03 +0800

    添加日志的TAG

--- mira/src/com/mira/MRLoginActivity.java
+++ mira/src/com/mira/MRLoginActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class MRLoginActivity extends Activity {
+	
+	private static String TAG = "MRLoginActivity";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
