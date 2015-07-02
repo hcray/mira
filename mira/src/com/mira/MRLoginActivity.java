d4271fe093c6023b895c3057c07491a4b1749373
caiyingying <caiyingying234@gmail.com>
2015-07-02 10:37:17 +0800

    登录页面的事件

--- mira/src/com/mira/MRLoginActivity.java
+++ mira/src/com/mira/MRLoginActivity.java
@@ -2,42 +2,98 @@ package com.mira;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.common.StringUtils;
 
 public class MRLoginActivity extends Activity {
 	
 	private static String TAG = "MRLoginActivity";
 	
+	/**
+	 * 账号
+	 */
 	private AutoCompleteTextView loginAccount;
 	
+	/**
+	 * 密码
+	 */
 	private EditText loginPassword;
 	
-	private Button loginButton;
+	/**
+	 * 登陆按钮
+	 */
+	private Button btn_login;
 	
-	private TextView forget;
+	/**
+	 * 忘记密码
+	 */
+	private TextView tvForget;
 	
-	private TextView signIn;
+	/**
+	 * 注册
+	 */
+	private TextView tvSignIn;
+	
+	private InputMethodManager imm;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.mr_activity_login);
+		Log.v(TAG, "onCreate()");
+		imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
 		
 		loginAccount = (AutoCompleteTextView) this.findViewById(R.id.login_account);
 		
 		loginPassword = (EditText) this.findViewById(R.id.login_password);
 		
-		loginButton = (Button) this.findViewById(R.id.login_activity_bt_login);
+		btn_login = (Button) this.findViewById(R.id.login_activity_bt_login);
 		
-		forget = (TextView) this.findViewById(R.id.login_activity_tv_forget);
+		tvForget = (TextView) this.findViewById(R.id.login_activity_tv_forget);
 		
-		signIn = (TextView) this.findViewById(R.id.login_activity_tv_signIn);
+		tvSignIn = (TextView) this.findViewById(R.id.login_activity_tv_signIn);
 		
+        btn_login.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				//隐藏软键盘
+				imm.hideSoftInputFromWindow(v.getWindowToken(), 0);  
+				//账号 密码
+				String account = loginAccount.getText().toString();
+				String pwd = loginPassword.getText().toString();
+				//判断输入
+				if(StringUtils.isEmpty(account)){
+					Toast.makeText(v.getContext(), getString(R.string.msg_login_account_null), Toast.LENGTH_SHORT).show();
+					return;
+				}
+				if(StringUtils.isEmpty(pwd)){
+					Toast.makeText(v.getContext(), getString(R.string.msg_login_pwd_null), Toast.LENGTH_SHORT).show();
+					return;
+				}
+			}
+		});
 		
-		
+        //忘记密码
+        tvForget.setOnClickListener(new View.OnClickListener() {
+     			public void onClick(View v) {
+     				//TODO
+     			}
+        });
+        //注册
+        tvSignIn.setOnClickListener(new View.OnClickListener() {
+        	public void onClick(View v) {
+        		//TODO
+        	}
+        });
+        
+        
 	}
 	
 	
