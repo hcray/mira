28d14815eab1b2797bdc1baa30671c2ef46e0fb1
caiyingying <caiyingying234@gmail.com>
2015-07-25 21:59:28 +0800

    去掉发现页签

--- mira/src/com/mira/MRMainActivity.java
+++ mira/src/com/mira/MRMainActivity.java
@@ -10,13 +10,12 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TabHost;
-import android.widget.Toast;
 import android.widget.TabHost.OnTabChangeListener;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.common.BaiDuLocationModel;
 import com.common.BaiduLocation;
-import com.common.DoubleClickExitHelper;
 import com.common.HandlerEvent;
 import com.common.MRCommon;
 import com.database.MRDataBase;
@@ -27,15 +26,15 @@ public class MRMainActivity extends TabActivity {
 	
 	TabHost tabHost;
 	ImageView ivIndex;
-	ImageView ivFind;
+	//ImageView ivFind;
 	ImageView ivTest;
 	ImageView ivMy;
 	LinearLayout llIndex;
-	LinearLayout llFind;
+	//LinearLayout llFind;
 	LinearLayout llTest;
 	LinearLayout llMy;
 	TextView tvIndex;
-	TextView tvFind;
+	//TextView tvFind;
 	TextView tvTest;
 	TextView tvMy;
 	@Override
@@ -45,15 +44,17 @@ public class MRMainActivity extends TabActivity {
 		MRDataBase.Init(this);
 		
 		ivIndex=(ImageView)findViewById(R.id.iv_index);
-		ivFind=(ImageView)findViewById(R.id.iv_find);
+		//ivFind=(ImageView)findViewById(R.id.iv_find);
 		ivTest=(ImageView)findViewById(R.id.iv_test);
 		ivMy=(ImageView)findViewById(R.id.iv_my);
+		
 		llIndex=(LinearLayout)findViewById(R.id.ll_index);
-		llFind=(LinearLayout)findViewById(R.id.ll_find);
+		//llFind=(LinearLayout)findViewById(R.id.ll_find);
 		llTest=(LinearLayout)findViewById(R.id.ll_test);
 		llMy=(LinearLayout)findViewById(R.id.ll_my);
+		
 		tvIndex=(TextView)findViewById(R.id.tv_index);
-		tvFind=(TextView)findViewById(R.id.tv_find);
+		//tvFind=(TextView)findViewById(R.id.tv_find);
 		tvTest=(TextView)findViewById(R.id.tv_test);
 		tvMy=(TextView)findViewById(R.id.tv_my);
 		View.OnClickListener ibClick=new View.OnClickListener() {
@@ -64,25 +65,26 @@ public class MRMainActivity extends TabActivity {
 				case R.id.ll_index:
 					tabHost.setCurrentTab(0);
 					ivIndex.setBackgroundResource(R.drawable.tab_infomation_select);
-					ivFind.setBackgroundResource(R.drawable.tab_discover);
+					//ivFind.setBackgroundResource(R.drawable.tab_discover);
 					ivTest.setBackgroundResource(R.drawable.tab_check);
 					ivMy.setBackgroundResource(R.drawable.tab_myinfo);
 					tvIndex.setTextColor(0xff63c5c8);
-					tvFind.setTextColor(0xff888888);
+					//tvFind.setTextColor(0xff888888);
 					tvTest.setTextColor(0xff888888);
 					tvMy.setTextColor(0xff888888);
 					break;
 				case R.id.ll_test:
 					tabHost.setCurrentTab(1);
 					ivIndex.setBackgroundResource(R.drawable.tab_infomation);
-					ivFind.setBackgroundResource(R.drawable.tab_discover);
+					//ivFind.setBackgroundResource(R.drawable.tab_discover);
 					ivTest.setBackgroundResource(R.drawable.tab_check_select);
 					ivMy.setBackgroundResource(R.drawable.tab_myinfo);
 					tvIndex.setTextColor(0xff888888);
-					tvFind.setTextColor(0xff888888);
+					//tvFind.setTextColor(0xff888888);
 					tvTest.setTextColor(0xff63c5c8);
 					tvMy.setTextColor(0xff888888);
 					break;
+				/*
 				case R.id.ll_find:
 					tabHost.setCurrentTab(2);	
 					ivIndex.setBackgroundResource(R.drawable.tab_infomation);
@@ -94,14 +96,15 @@ public class MRMainActivity extends TabActivity {
 					tvTest.setTextColor(0xff888888);
 					tvMy.setTextColor(0xff888888);
 					break;
+				*/
 				case R.id.ll_my:
-					tabHost.setCurrentTab(3);
+					tabHost.setCurrentTab(2);
 					ivIndex.setBackgroundResource(R.drawable.tab_infomation);
-					ivFind.setBackgroundResource(R.drawable.tab_discover);
+					//ivFind.setBackgroundResource(R.drawable.tab_discover);
 					ivTest.setBackgroundResource(R.drawable.tab_check);
 					ivMy.setBackgroundResource(R.drawable.tab_myinfo_select);
 					tvIndex.setTextColor(0xff888888);
-					tvFind.setTextColor(0xff888888);
+					//tvFind.setTextColor(0xff888888);
 					tvTest.setTextColor(0xff888888);
 					tvMy.setTextColor(0xff63c5c8);
 					break;
@@ -113,14 +116,15 @@ public class MRMainActivity extends TabActivity {
 			}
 		};
 		llIndex.setOnClickListener(ibClick);
-		llFind.setOnClickListener(ibClick);
+		//llFind.setOnClickListener(ibClick);
 		llTest.setOnClickListener(ibClick);
 		llMy.setOnClickListener(ibClick);
+	
 		tabHost = getTabHost();
 		tabHost.addTab(tabHost.newTabSpec("a").setIndicator("0").setContent(new Intent(MRMainActivity.this, MRIndexActivity.class)));
 		tabHost.addTab(tabHost.newTabSpec("b").setIndicator("1").setContent(new Intent(MRMainActivity.this, MRTestActivity.class)));
-		tabHost.addTab(tabHost.newTabSpec("c").setIndicator("2").setContent(new Intent(MRMainActivity.this, MRFindActivity.class)));
-		tabHost.addTab(tabHost.newTabSpec("d").setIndicator("3").setContent(new Intent(MRMainActivity.this, MRMyActivity.class)));
+		//tabHost.addTab(tabHost.newTabSpec("c").setIndicator("2").setContent(new Intent(MRMainActivity.this, MRFindActivity.class)));
+		tabHost.addTab(tabHost.newTabSpec("c").setIndicator("2").setContent(new Intent(MRMainActivity.this, MRMyActivity.class)));
 		tabHost.setOnTabChangedListener(new OnTabChangeListener() {
 			
 			@Override
@@ -131,11 +135,12 @@ public class MRMainActivity extends TabActivity {
 		});
 		tabHost.setCurrentTab(0);
 		ivIndex.setBackgroundResource(R.drawable.tab_infomation_select);
-		ivFind.setBackgroundResource(R.drawable.tab_discover);
+		//ivFind.setBackgroundResource(R.drawable.tab_discover);
 		ivTest.setBackgroundResource(R.drawable.tab_check);
 		ivMy.setBackgroundResource(R.drawable.tab_myinfo);
+		
 		tvIndex.setTextColor(0xff63c5c8);
-		tvFind.setTextColor(0xff888888);
+		//tvFind.setTextColor(0xff888888);
 		tvTest.setTextColor(0xff888888);
 		tvMy.setTextColor(0xff888888);
 		MRCommon.update(this);
