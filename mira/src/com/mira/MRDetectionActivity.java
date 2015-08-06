148a56334e3a0ead8b5eafbb547654326888ceb5
caiyingying <caiyingying234@gmail.com>
2015-08-06 22:24:13 +0800

    检测页面的首页

---
+++ mira/src/com/mira/MRDetectionActivity.java
@@ -0,0 +1,15 @@
+package com.mira;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MRDetectionActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.mr_activity_detection);
+	}
+}
