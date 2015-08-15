d3a8bf93a7662c3210532e53c0949d223015769a
caiyingying <caiyingying234@gmail.com>
2015-08-15 10:55:36 +0800

    常量更新

--- mira/src/com/common/MiraConstants.java
+++ mira/src/com/common/MiraConstants.java
@@ -10,65 +10,65 @@ public class MiraConstants {
 	/**
 	 * 服务端地址
 	 */
-	public final static String baseUrl = "";
+	public final static String baseUrl = "http://";
 	
 	/**
 	 * 创建Token
 	 */
-	public final static String createToken = "http://" + baseUrl + "/api/User/CreateToken";
+	public final static String createToken = baseUrl + "/api/User/CreateToken";
 	
 	/**
 	 * 用户登录
 	 */
-	public final static String userLogin = "http://" + baseUrl + "/api/User/Login";
+	public final static String userLogin = baseUrl + "/api/User/Login";
 	
 	/**
 	 * 获取验证码
 	 */
-	public final static String getVerificationCode = "http://" + baseUrl + "/api/User/GetVerificationCode";
+	public final static String getVerificationCode = baseUrl + "/api/User/GetVerificationCode";
 	
 	/**
 	 * 修改个人信息
 	 */
-	public final static String updateUserInfo = "http://" + baseUrl + "/api/User/UpdateUserInfo";
+	public final static String updateUserInfo = baseUrl + "/api/User/UpdateUserInfo";
 	
 	/**
 	 * 修改密码
 	 */
-	public final static String changePassword = "http://" + baseUrl + "/api/User/ChangePassword";
+	public final static String changePassword = baseUrl + "/api/User/ChangePassword";
 	
 	/**
 	 * 获取皮肤类型小类
 	 */
-	public final static String getSkinList = "http://" + baseUrl + "/api/User/GetSkinList";
+	public final static String getSkinList = baseUrl + "/api/User/GetSkinList";
 	
 	/**
 	 * 获取敏感类型小类
 	 */
-	public final static String getSensitiveList = "http://" + baseUrl + "/api/User/GetSensitiveList";
+	public final static String getSensitiveList = baseUrl + "/api/User/GetSensitiveList";
 
 	/**
 	 * 上传图片接口
 	 */
-	public final static String uploadImageByte = "http://" + baseUrl + "/api/User/UploadImageByte";
+	public final static String uploadImageByte = baseUrl + "/api/User/UploadImageByte";
 
 	/**
 	 * 上传图片信息接口
 	 */
-	public final static String uploadImage = "http://" + baseUrl + "/api/User/UploadImage";
+	public final static String uploadImage = baseUrl + "/api/User/UploadImage";
 
 	/**
 	 * 上传检测数据
 	 */
-	public final static String uploadDetection = "http://" + baseUrl + "/api/User/UploadDetection";
+	public final static String uploadDetection = baseUrl + "/api/User/UploadDetection";
 
 	/**
 	 * 下载检测数据
 	 */
-	public final static String getDetection = "http://" + baseUrl + "/api/User/GetDetection";
+	public final static String getDetection = baseUrl + "/api/User/GetDetection";
 
 	/**
 	 * 上传日志接口
 	 */
-	public final static String uploadNotice = "http://" + baseUrl + "/api/User/UploadNotice";
+	public final static String uploadNotice = baseUrl + "/api/User/UploadNotice";
 }
